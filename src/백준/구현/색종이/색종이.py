f3c7e445a0c378a3805a6253402ee7d516bd7436
choikang <choikang7157@naver.com>
2026-02-13 14:42:26 +0900

    백준 - 색종이 문제 풀이(02_13)

---
+++ src/백준/구현/색종이/색종이.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+boards=[[0]*101 for _ in range(101)]
+
+t=int(input())
+count=0
+for _ in range(t):
+    x,y=map(int,input().split())
+
+    for i in range(x,x+10):
+        for j in range(y,y+10):
+            boards[i][j]=1
+
+for i in range(101):
+    for j in range(101):
+        if boards[i][j]==1:
+            count+=1
+
+print(count)
